df78e441e50f7e16ca6561bfc30e89905a8f0302
Thomas <tray8paterson@gmail.com>
2025-11-28 23:52:38 -0600

    started playing around with events and updated collectables

--- Assets/Scripts/CollectableScript.cs
+++ Assets/Scripts/CollectableScript.cs
@@ -1,11 +1,13 @@
+using System;
 using UnityEngine;
 
 
 public class CollectableScript : MonoBehaviour
 {
-    public GameObject player;
     private GameObject currObj;
 
+    public static event Action OnCollected;//event that can be activated and referanced elsewhere 
+    //it is possible we could make an event for each "power"
     public string typeOfCollectable;//replace this with an enum later if you can
 
     private bool state = true;//true == able to be collected, false == unable to be collected
@@ -17,24 +19,18 @@ public class CollectableScript : MonoBehaviour
 
     void Update()
     {
-        if (state)
-        {
-            currObj.SetActive(true);
-        }
-        else
-        {
-            currObj.SetActive(false);
-        }
+        transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
+        currObj.SetActive(state);
     }
 
-    private void OnTriggerEnter(Collider colided)
+    void OnTriggerEnter(Collider colided)
     {
-
-        if(colided.tag == "Player")
+        if(colided.CompareTag("Player") && state)
         {
             state = false;
+            OnCollected?.Invoke();
+            //the ? means the same thing as if(OnCollected != null)
         }
-
     }
 
 }
