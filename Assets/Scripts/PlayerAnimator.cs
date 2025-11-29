c03356a3a64a915d34d1c18aa4c285afe30ef81b
Gev <105464495+GevCreates@users.noreply.github.com>
2025-11-29 14:03:44 -0600

    Character Animation Blending, Leg Scaling

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -2,12 +2,28 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
+    enum AnimationState {
+        Idle,
+        Run,
+        Jump
+    }
+
     private const string ANIM_IDLE = "char_idle";
     private const string ANIM_RUN = "char_run";
     private const string ANIM_JUMP = "char_jump";
 
+    private const float CHARACTER_Y_BASE = -1.075f;
+    private const float CHARACTER_Y_EXTEND_SCALE = 1f;
+
+    private const int BLENDSHAPE_LONGLEGS = 0;
+
+    private new Animation animation;
+    private AnimationState animationState = AnimationState.Idle;
+
     public PlayerMovement player;
-    private Animation animation;
+    public SkinnedMeshRenderer characterMesh;
+    [Range(0f, 1f)]
+    public float legScale = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,17 +34,42 @@ public class PlayerAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        characterMesh.SetBlendShapeWeight(BLENDSHAPE_LONGLEGS, legScale * 100f);
+        gameObject.transform.position = new Vector3(
+                player.gameObject.transform.position.x, 
+                player.gameObject.transform.position.y + CHARACTER_Y_BASE + legScale * CHARACTER_Y_EXTEND_SCALE, 
+                player.gameObject.transform.position.z
+            );
+
         if (player.grounded) {
             Vector3 xzMovement = Vector3.Scale(player.finalMove, new Vector3(1, 0, 1));
             if (xzMovement.magnitude > 0.0) {
-                animation.Play(ANIM_RUN);
+                if (animationState != AnimationState.Run) {
+                    animationState = AnimationState.Run;
+                    animation.Blend(ANIM_RUN);
+
+                    animation.Blend(ANIM_IDLE, 0.0f);
+                    animation.Blend(ANIM_JUMP, 0.0f);
+                }
             }
             else {
-                animation.Play(ANIM_IDLE);
+                if (animationState != AnimationState.Idle) {
+                    animationState = AnimationState.Idle;
+                    animation.Blend(ANIM_IDLE);
+                    
+                    animation.Blend(ANIM_RUN, 0.0f);
+                    animation.Blend(ANIM_JUMP, 0.0f);
+                }
             }
         }
         else {
-            animation.Play(ANIM_JUMP);
+            if (animationState != AnimationState.Jump) {
+                animationState = AnimationState.Jump;
+                animation.Blend(ANIM_JUMP);
+                    
+                animation.Blend(ANIM_IDLE, 0.0f);
+                animation.Blend(ANIM_RUN, 0.0f);
+            }
         }
     }
 }
