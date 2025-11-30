2fa6b23d0ddf3d1103f48e0694611877ab8fa2ce
Gev <105464495+GevCreates@users.noreply.github.com>
2025-11-30 10:02:51 -0600

    More low poly stuff

---
+++ Assets/Scripts/AutoTile.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AutoTile : MonoBehaviour
+{
+    public float textureScale = 0.5f;
+
+    private Material material;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        material = gameObject.GetComponent<Renderer>().material;
+        material.SetTextureScale("_BaseMap", new Vector2(transform.localScale.x * textureScale, transform.localScale.y * textureScale));
+    }
+
+}
