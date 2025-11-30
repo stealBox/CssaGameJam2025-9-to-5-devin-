562ba55431e1741b18f9ef9d376bf4e827ea08aa
Itiodic-dev <itsbusinessingtime@gmail.com>
2025-11-30 13:21:17 -0600

    Enable cursor for credits

---
+++ Assets/Scripts/EnableCursor.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class EnableCursor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
