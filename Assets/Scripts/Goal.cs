b77123f38190d7a0a69626520da86e66154aa76b
Thomas <tray8paterson@gmail.com>
2025-11-29 19:50:52 -0600

    added a field of spikes and increased the difficulty of some jumps

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,7 +4,6 @@ public class Goal : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision deteced");
         if (other.gameObject.CompareTag("Player"))
         {
             PlayerManager.instance.WinGame();
